a4e7ab240501c11ec03c2f2938094abcfce7a4a6
vishal5251 <vishaljaiswal9551@gmail.com>
2026-03-27 10:34:43 +0530

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:5000/api/comparisons';
-const BASE_URL = `/api/comparisons`;
+const BASE_URL = `${import.meta.env.Server_Base_Url}/api/comparisons`;
 
 export const saveComparison = async (data) => {
   console.log("Base_url: ", BASE_URL)
